66cf3394cb28cbdb6eba791da93d56945145be81
Allen Masud <100032855+allenmasud12@users.noreply.github.com>
2024-06-01 15:59:53 +0600

    image field add to model and html page

--- foodapp/models.py
+++ foodapp/models.py
@@ -4,6 +4,7 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200, default="")
     item_des = models.CharField(max_length=200, default="")
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://alllocal.ca/wp-content/uploads/2020/11/food-placeholder.png")
 
     def __str__(self):
         return self.item_name
