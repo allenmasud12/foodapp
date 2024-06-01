febff723db684e01cb3ec95d6edd89e5ba517c4d
Allen Masud <100032855+allenmasud12@users.noreply.github.com>
2024-06-01 22:24:31 +0600

    delete method

--- foodapp/urls.py
+++ foodapp/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
         path('item/', views.item, name = 'item'),   
         path('<int:item_id>/', views.detail, name='detail'), 
         #/add item
-        path('add/', views.create_item, name= 'create_item')
+        path('add/', views.create_item, name= 'create_item'),
+        #/edit item
+        path('update/<int:id>/', views.update_item, name='update_item'),
+        #/delete item
+        path('delete/<int:id>/', views.delete_item, name= 'delete_item'),
     ]
 
