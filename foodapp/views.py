ed1a1f94addf31b8172d9a2e6285449cfe8eab99
Allen Masud <100032855+allenmasud12@users.noreply.github.com>
2024-06-01 13:51:23 +0600

    templates and navbar added showing database data

--- foodapp/views.py
+++ foodapp/views.py
@@ -1,3 +1,24 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Item
+from django.template import loader
+def index(request):
+    list_item = Item.objects.all()
+    # template =loader.get_template('index.html')
+    context ={
+        'item_list': list_item,
+    }
+    
+    # return HttpResponse(template.render(context, request))
+    return render(request,'index.html', context)
+    
 
-# Create your views here.
+def item(request):
+    return HttpResponse("this is two page")
+
+def detail(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    context ={
+        'item': item
+    }
+    return render(request, "detail.html", context)
